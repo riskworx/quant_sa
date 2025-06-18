541c7b7721f564ea2e2401d2bb3921cb9d32e7d3
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-06-18 16:49:33 +0200

    Add unit test for Calender.BusinessDaysBetween method.

---
+++ QuantSA/QuantSA.Shared.Test/Dates/CalenderTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantSA.Shared.Dates;
+using Calendar = QuantSA.Shared.Dates.Calendar;
+
+namespace QuantSA.Shared.Test.Dates
+{
+    [TestClass]
+    public class CalenderTests
+    {
+        [TestMethod]
+        public void BusinessDaysBetween_ExcludesWeekends()
+        {
+            var calender = new Calendar("TestCalender");
+            var start = new Date(2024, 6, 3);
+            var end = new Date(2025, 6, 7);
+            int result = calender.BusinessDaysBetween(start, end);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
