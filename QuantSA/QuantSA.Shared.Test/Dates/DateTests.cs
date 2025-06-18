af4c69c9e51d7d1f486a613305ef63c66f9da1fb
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-06-18 16:17:58 +0200

    Add unit test for Date constructor to test basic date creation.

---
+++ QuantSA/QuantSA.Shared.Test/Dates/DateTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantSA.Shared.Dates;
+using System;
+
+
+namespace QuantSA.Shared.Test.Dates
+{
+    [TestClass]
+    public class DateTest
+    {
+        [TestMethod]
+        public void Function_FromYearMonthDay_InitializesCorrectly()
+        {
+            var date = new Date(2023, 12, 25);
+            Assert.AreEqual(2023, date.Year);
+            Assert.AreEqual(12, date.Month);
+            Assert.AreEqual(25, date.Day);
+        }
+
+    }
+}
+
