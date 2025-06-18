1189565889af0c2d6525cfd3a7bf4bfbd54d02c4
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-06-18 22:08:13 +0200

    Add unit test to Zero-Coupon Bonds: QuantSA/Core/Tests/Rates/ZeroCouponBondTests.cs

---
+++ QuantSA/QuantSA.Core.Tests/Products/Rates/ZeroCouponBondTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantSA.Core.Products.Rates;
+using QuantSA.Shared.Dates;
+using QuantSA.Shared.Primitives;
+
+namespace QuantSA.Core.Tests.Products.Rates
+{
+    [TestClass]
+    public class ZeroCouponBondTests
+    {
+        [TestMethod]
+        public void ZeroCouponBond_ReturnsSingleCashflow()
+        {
+            // Arrange
+            var maturity = new Date(2028, 12, 31);
+            double notional = 1_000_000;
+            var currency = new Currency("ZAR");
+
+            var bond = new ZeroCouponBond(maturity, notional, currency);
+
+            // Act
+            var cashflows = bond.GetCFs();
+
+            // Assert
+            Assert.AreEqual(1, cashflows.Count);
+            Assert.AreEqual(maturity, cashflows[0].Date);
+            Assert.AreEqual(notional, cashflows[0].Amount);
+            Assert.AreEqual(currency, cashflows[0].Currency);
+        }
+    }
+}
