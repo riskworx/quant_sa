b217d3ca52531634626583ab64285ea5ad03fb62
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-06-23 15:33:05 +0200

    Add IInterpolator1D.cs interface for generic 1D interpolation.

---
+++ QuantSA/QuantSA.Core/CurvesAndSurfaces/IInterpolator1D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantSA.Core.CurvesAndSurfaces
+{
+    public interface IInterpolator1D
+    {
+        double Interpolate(double x);
+    }
+}
