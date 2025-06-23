a96fb7638a6b82aeaae0991b21dec0673f38a2d6
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-06-23 15:34:11 +0200

    Add LinearSplineAdapter to wrap Math.NET LinearSpline as IInterpolator1D.

---
+++ QuantSA/QuantSA.Core/CurvesAndSurfaces/InterpolationAdapters/InterpolationAdapters.cs
@@ -0,0 +1,19 @@
+﻿using MathNet.Numerics.Interpolation;
+
+namespace QuantSA.Core.CurvesAndSurfaces
+{
+    public class LinearSplineAdapter : IInterpolator1D
+    {
+        private readonly LinearSpline _spline;
+
+        public LinearSplineAdapter(double[] xVals, double[] yVals)
+        {
+            _spline = LinearSpline.InterpolateSorted(xVals, yVals);
+        }
+
+        public double Interpolate(double x)
+        {
+            return _spline.Interpolate(x);
+        }
+    }
+}
