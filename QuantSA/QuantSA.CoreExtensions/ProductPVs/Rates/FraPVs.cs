d4e9cb85db7dc80217eaa7477eb0c23a2ba7dff4
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-06-23 12:42:16 +0200

    Add Present Value calculation for FRA.

---
+++ QuantSA/QuantSA.CoreExtensions/ProductPVs/Rates/FraPVs.cs
@@ -0,0 +1,18 @@
+﻿using QuantSA.Core.Primitives;
+using QuantSA.Core.Products.Rates;
+using QuantSA.Shared.MarketData;
+
+namespace QuantSA.CoreExtensions.ProductPVs.Rates
+{
+    public static class FraPVs
+    {
+        /// <summary>
+        /// Values the FRA by discounting the generated cashflow.
+        /// </summary>
+        public static double CurvePv(this FRA fra, IDiscountingSource discountCurve)
+        {
+            var cfs = fra.GetCFs();  // internally uses _nearDate, _rate, _accrualFraction etc.
+            return cfs.PV(discountCurve);  // extension method in QuantSA
+        }
+    }
+}
