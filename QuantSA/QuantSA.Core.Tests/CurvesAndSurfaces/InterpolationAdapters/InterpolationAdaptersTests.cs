33d2a93d3f8a51fa16580d015bea627cf5b35249
MarcelleOberholzer <marcelle.oberholzer@riskworx.com>
2025-07-03 16:41:09 +0200

    Add tests that lie on the left and right of the boundaries.

--- QuantSA/QuantSA.Core.Tests/CurvesAndSurfaces/InterpolationAdapters/InterpolationAdaptersTests.cs
+++ QuantSA/QuantSA.Core.Tests/CurvesAndSurfaces/InterpolationAdapters/InterpolationAdaptersTests.cs
@@ -7,16 +7,43 @@ namespace QuantSA.Core.Tests.CurvesAndSurfaces.InterpolationAdapters
     [TestClass]
     public class InterpolatedCurveTests
     {
+        private double[] _xVals;
+        private double[] _yVals;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _xVals = new double[] { 1, 2, 3 };
+            _yVals = new double[] { 10, 20, 30 };
+        }
         [TestMethod]
         public void InterpolatedCurve_ReturnsValues_AsExpected()
         {
-            double[] xVals = { 1, 2, 3 };
-            double[] yVals = { 10, 20, 30 };
-            var curve = new InterpolatedCurve(xVals, yVals);
+            var curve = new InterpolatedCurve(_xVals, _yVals);
 
             double valueAt1_5 = curve.Interp(1.5);
 
             Assert.AreEqual(15, valueAt1_5, 1e-10, "Interpolation failed");
+        }
+
+        [TestMethod]
+        public void InterpolatedCurve_ExtrapolatesLeft_ReturnsASExpected()
+        {
+            var curve = new InterpolatedCurve(_xVals, _yVals);
+
+            double valueAt0_5 = curve.Interp(0.5);
+
+            Assert.AreEqual(5, valueAt0_5, 1e-10, "Left extrapolation failed");
+        }
+
+        [TestMethod]
+        public void InterpolatedCurve_ExtrapolatesRight_ReturnsASExpected()
+        {
+            var curve = new InterpolatedCurve(_xVals, _yVals);
+
+            double valueAt3_5 = curve.Interp(3.5);
+
+            Assert.AreEqual(35, valueAt3_5, 1e-10, "Right extrapolation failed");
         }
     }
 }
